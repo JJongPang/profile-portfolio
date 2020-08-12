9b90c3fe486aca50c7fac37aab0ef3125a51b917
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-08-13 00:18:49 +0900

    JS Navbar menu click

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 'use strict'
 
-//Make Navbar transparent when it is on the top
+
 const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height; //navbar height check
 
-// navbar 높이 확인
-const navbarHeight = navbar.getBoundingClientRect().height;
-
+//Make Navbar transparent when it is on the top
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
@@ -14,4 +13,15 @@ document.addEventListener('scroll', () => {
     }
 });
 
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
 
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+});
